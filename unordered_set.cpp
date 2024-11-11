9f0dfd434fbbb1fc90a2d1730977b2fdd52d5efb
Rithindatta Gundu <rithindatta.g@gmail.com>
2024-11-11 00:23:22 -0800

    Adding unordered_set.cpp

---
+++ unordered_set.cpp
@@ -0,0 +1,11 @@
+// C++ STL - Unordered Set
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> st; // Unique elements without any specified order.
+
+    // All functions in 'set' works for 'unordered_set'
+}
